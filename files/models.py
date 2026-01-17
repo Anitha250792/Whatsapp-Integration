0236774d3a425d1076a36305af66a2b663a4198b
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-17 06:32:13 +0530

    Add FileSerializer and fix missing serializers module

--- files/models.py
+++ files/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class UploadedFile(models.Model):
+
+class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to="uploads/")
+    filename = models.CharField(max_length=255)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
-    @property
-    def filename(self):
-        return self.file.name.split("/")[-1]
+    def __str__(self):
+        return self.filename
