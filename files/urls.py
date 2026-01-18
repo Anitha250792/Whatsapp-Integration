1e0b272e45fb5fe7dbee4d8a88ee4ae851cb9236
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-18 17:28:07 +0530

    Fix urlpatterns typo in config urls

--- files/urls.py
+++ files/urls.py
@@ -16,10 +16,8 @@ urlpatterns = [
     path("upload/", UploadFileView.as_view(), name="file-upload"),
     path("delete/<int:file_id>/", DeleteFileView.as_view()),
     path("download/<int:file_id>/", DownloadFileView.as_view()),
-
     path("convert/word-to-pdf/<int:file_id>/", WordToPDFView.as_view()),
     path("convert/pdf-to-word/<int:file_id>/", PDFToWordView.as_view()),
-
     path("merge/", MergePDFView.as_view()),
     path("split/<int:file_id>/", SplitPDFView.as_view()),
     path("sign/<int:file_id>/", SignPDFView.as_view()),
