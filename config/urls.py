bb5216dbaa05769f81c3ba5404c9a477c3343ae1
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-18 17:50:12 +0530

    Fix urlpatterns config urls

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include("dj_rest_auth.urls")),
-    path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("auth/", include("dj_rest_auth.urls")),
+    path("auth/registration/", include("dj_rest_auth.registration.urls")),
     path("accounts/", include("allauth.urls")),
     path("files/", include("files.urls")),
 ]
