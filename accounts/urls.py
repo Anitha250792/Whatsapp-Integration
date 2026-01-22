3f487286a763bc857d814af30f7efc5ca6aff296
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-22 11:48:24 +0530

    config,accounts  urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("google/", GoogleLoginAPIView.as_view()),
+    path("", GoogleLoginAPIView.as_view()),
     path("privacy-policy/", privacy_policy, name="privacy_policy"),
     path("terms/", terms_of_service, name="terms"),
     path("data-deletion/", data_deletion_view),
