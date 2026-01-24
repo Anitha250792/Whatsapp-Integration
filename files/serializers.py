fa8a3396bf4baf8978d4f68e5a92109a669564f7
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-24 11:09:22 +0530

    file upload fix

--- files/serializers.py
+++ files/serializers.py
@@ -18,8 +18,11 @@ class FileSerializer(serializers.ModelSerializer):
             reverse("file-download", args=[obj.id])
         )
 
-    def get_public_url(self, obj):
-        request = self.context.get("request")
-        return request.build_absolute_uri(
-            reverse("file-public", args=[obj.public_token])
-        )
+def get_public_url(self, obj):
+    request = self.context.get("request")
+    if not hasattr(obj, "public_token") or not obj.public_token:
+        return None
+
+    return request.build_absolute_uri(
+        reverse("file-public", args=[obj.public_token])
+    )
