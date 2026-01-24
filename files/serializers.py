a6b4c6f88c228b0aed1fb7f6f99c22f6154cf621
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-24 16:45:35 +0530

    file download fix error

--- files/serializers.py
+++ files/serializers.py
@@ -4,19 +4,18 @@ from django.urls import reverse
 from .models import File
 
 
-# files/serializers.py
 class FileSerializer(serializers.ModelSerializer):
-    public_url = serializers.SerializerMethodField()
+    download_url = serializers.SerializerMethodField()
 
     class Meta:
         model = File
-        fields = ["id", "filename", "public_url"]
+        fields = ["id", "filename", "download_url"]
 
-    def get_public_url(self, obj):
+    def get_download_url(self, obj):
         request = self.context.get("request")
-        if not request:
+        if request is None:
             return None
-        return request.build_absolute_uri(
-            reverse("file-public", args=[obj.public_token])
-        )
 
+        return request.build_absolute_uri(
+            reverse("file-download", args=[obj.id])
+        )
