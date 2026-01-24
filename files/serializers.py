9561360ffcbcef4d4883f43948b14ff309f2810d
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-24 16:06:27 +0530

    upload fix

--- files/serializers.py
+++ files/serializers.py
@@ -3,25 +3,19 @@ from rest_framework import serializers
 from django.urls import reverse
 from .models import File
 
+
 class FileSerializer(serializers.ModelSerializer):
     download_url = serializers.SerializerMethodField()
-    public_url = serializers.SerializerMethodField()
 
     class Meta:
         model = File
-        fields = ["id", "filename", "download_url", "public_url"]
+        fields = ["id", "filename", "download_url"]
 
     def get_download_url(self, obj):
         request = self.context.get("request")
-        return request.build_absolute_uri(
-            reverse("file-download", args=[obj.id])
-        )
-
-    def get_public_url(self, obj):
-        request = self.context.get("request")
-        if not obj.public_token:
+        if request is None:
             return None
 
         return request.build_absolute_uri(
-            reverse("file-public", args=[obj.public_token])
+            reverse("file-download", args=[obj.id])
         )
