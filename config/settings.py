06f7f76c3d2e0ba431f665ee54c4501724c348aa
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-24 12:06:42 +0530

    user name

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
 
+
     # Third-party
     "corsheaders",
     "rest_framework",
--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,14 @@ from django.conf.urls.static import static
 from accounts import views as account_views
 
 urlpatterns = [
-     path("privacy-policy/", account_views.privacy_policy),
+    path("privacy-policy/", account_views.privacy_policy),
     path("terms/", account_views.terms_of_service),
     path("data-deletion/", account_views.data_deletion_view),
     path("admin/", admin.site.urls),
+
+    # 🔐 AUTH
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 
     # 🔐 JWT auth (email/password)
     path("auth/", include("dj_rest_auth.urls")),
@@ -16,8 +20,6 @@ urlpatterns = [
 
     # 🔐 Google JWT login (custom API)
     path("accounts/", include("accounts.urls")),
-
-   
 
     # 📁 File APIs (NO /api)
     path("files/", include("files.urls")),
