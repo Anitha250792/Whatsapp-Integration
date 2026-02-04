86133107c259cfba97674626462b8da735ad7c6e
Anitha250792 <ntanithasaravanan@gmail.com>
2026-02-04 11:35:08 +0530

    env,whatsapp

--- files/whatsapp.py
+++ files/whatsapp.py
@@ -1,22 +1,24 @@
-# files/whatsapp.py
 import requests
 from django.conf import settings
 
 def send_whatsapp_message(to_number, message):
-    url = f"https://graph.facebook.com/v18.0/{settings.WHATSAPP_PHONE_NUMBER_ID}/messages"
+    url = f"https://graph.facebook.com/v19.0/{settings.922813980922652}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {settings.EAARrIXLNDUwBQmlPZAylIUL0jVYk79ilHijvm9Cp5RYzQfJHxp1E2wWbSIsAeZAVFsOdP4jwmmmbTeWenzgy4veWrHMpxf9Ptsra2qOHaBk2itoS8oZBsIxAZB8aU0BiXl4Ut7z0DFRdWAb6uDdY23tc6YUbKT2qrgLgN6McB31Ws6f6W0RxZBUDeUSs7P5oLVuhM2jZBJwB4RaZBfOhtfL5NaNJeezNNhlC2DjTEx01wEDjhjA5dRoIPAUXtGjIfCMTv4ITB3kFmg1XRGk5FsrY8gvnzZAo2O4TGwgNQwZDZD}",
+        "Content-Type": "application/json",
+    }
 
     payload = {
         "messaging_product": "whatsapp",
         "to": to_number,
         "type": "text",
-        "text": {"body": message}
-    }
-
-    headers = {
-        "Authorization": f"Bearer {settings.WHATSAPP_ACCESS_TOKEN}",
-        "Content-Type": "application/json",
+        "text": {"body": message},
     }
 
     response = requests.post(url, json=payload, headers=headers)
-    response.raise_for_status()
+
+    if response.status_code >= 400:
+        raise Exception(response.text)
+
     return response.json()
