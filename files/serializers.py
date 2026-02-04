dc1b2157a5d036f5952eb86a2b175b379eafe0a4
Anitha250792 <ntanithasaravanan@gmail.com>
2026-02-04 16:22:47 +0530

    serializers fix

--- files/serializers.py
+++ files/serializers.py
@@ -1,6 +1,4 @@
-# files/serializers.py
 from rest_framework import serializers
-from django.urls import reverse
 from .models import File
 
 class FileSerializer(serializers.ModelSerializer):
@@ -8,10 +6,18 @@ class FileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = File
-        fields = ["id", "filename", "public_token", "public_url"]
+        fields = [
+            "id",
+            "filename",
+            "public_token",
+            "public_url",
+        ]
 
     def get_public_url(self, obj):
         request = self.context.get("request")
+        if request is None:
+            return None
+
         return request.build_absolute_uri(
             f"/files/public/{obj.public_token}/"
         )
