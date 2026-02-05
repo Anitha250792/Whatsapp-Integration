a7251fe071c0a1f573189156357c2fe005e22272
Anitha250792 <ntanithasaravanan@gmail.com>
2026-02-05 09:45:41 +0530

    celery correction

--- config/__init__.py
+++ config/__init__.py
@@ -1,2 +1,7 @@
-from .celery import app as celery_app
+# Celery is optional on web deployments
+try:
+    from .celery import app as celery_app
+except ImportError:
+    celery_app = None
+
 __all__ = ("celery_app",)
